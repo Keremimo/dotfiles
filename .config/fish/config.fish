58c17e2b7cb2628da6fa576f303888f6854e8bbe
keremimo <nyaa@live.com>
2024-07-15 20:14:47 +0200

    Change oh-my-posh theme to kushal

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 function fish_greeting
-  kitten icat --align left ~/Pictures/okay.png
+  #kitten icat --align left ~/Pictures/okay.png
 end
-oh-my-posh init fish | source
+oh-my-posh init fish --config /usr/share/oh-my-posh/themes/kushal.omp.json | source
 
 fish_add_path /home/kerem/.spicetify
 zoxide init fish | source
 alias cd=z
 ~/.local/bin/mise activate fish | source
+
+thefuck --alias | source
