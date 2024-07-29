08d6192ab5d861da9e6b772c20b3382f6ca1ff71
keremimo <nyaa@live.com>
2024-07-29 11:49:39 +0200

    Update: switch to hyprland

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,6 @@ end
 function fish_greeting
   #kitten icat --align left ~/Pictures/okay.png
 end
-#oh-my-posh init fish --config /usr/share/oh-my-posh/themes/kushal.omp.json | source
 starship init fish | source
 fish_add_path /home/kerem/.spicetify
 zoxide init fish | source
