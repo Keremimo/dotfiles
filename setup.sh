d4353f3f08df07817a64addfe8d69feb8de7ec41
Kerem Kiliç <nyaa@live.com>
2024-10-25 20:52:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,3 +28,10 @@ cp -rf ~/dotfiles/Wallpapers/* ~/Wallpapers
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 cd .bash_it
 ./install.sh
+echo "y"
+echo "y"
+bash-it enable plugin fzf zoxide alias-completion
+cd
+git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
+make -C ble.sh install PREFIX=~/.local
+echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
