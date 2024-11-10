616962a458899e65eb82ec0d0300457d59363f71
Keremimo <nyaa@live.com>
2024-11-10 18:52:46 +0100

    Fish now has vi bindings, modified borders, fixed bugs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,15 @@ alias ls=eza
 
 thefuck --alias | source
 
+function fish_hybrid_key_bindings --description \
+"Vi-style bindings that inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+set -g fish_key_bindings fish_hybrid_key_bindings
+
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
