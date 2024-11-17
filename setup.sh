f876327b9dd1e6a23b761228aa6adba819bba6bd
Kerem Kiliç <nyaa@live.com>
2024-11-17 21:16:10 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo systemctl enable --now bluetooth
 sudo pacman -S --noconfirm --needed wl-clipboard
 
 ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
-cat ~/.ssh/id_ed25519.pub | wl-copy
+#cat ~/.ssh/id_ed25519.pub | wl-copy
 read -rp "Press enter to continue after inputting your ssh key into github."
 
 sudo pacman -S --noconfirm --needed base-devel stow lua fd yazi pipewire dunst noto-fonts-emoji libgtop bluez bluez-utils btop networkmanager brightnessctl python gnome-bluetooth-3.0 pacman-contrib gvfs tlp hyprland hyprpaper pavucontrol nm-connection-editor swww fish starship unzip git curl wget bash-completion fuzzel rustup go neovim kitty fzf polkit chromium nerd-fonts gtk3 gtk4 sddm zellij thefuck zoxide nodejs yarn npm gnome-keyring flatpak udiskie qt5-wayland qt6-wayland &&
@@ -24,7 +24,7 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si --noconfirm
 cd ..
-git clone git@github.com:Keremimo/dotfiles.git
+git clone https://github.com/Keremimo/dotfiles.git
 cd ~/dotfiles
 stow . --adopt
 cd
