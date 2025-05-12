6f8d610da76ecc3a974e933c3f3e4e950bbe2e2a
Kerem Kiliç <code@kerem.tech>
2025-05-12 20:49:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
 cat ~/.ssh/id_ed25519.pub | wl-copy
 read -n 1 -p "Press 1 to continue after inputting your ssh key into github."
 
-sudo pacman -S --noconfirm --needed base-devel stow ghostty lua fd ripgrep yazi eza pipewire dunst noto-fonts-emoji libgtop bluez bluez-utils btop networkmanager brightnessctl python gnome-bluetooth-3.0 pacman-contrib gvfs tlp hyprland hyprpaper pavucontrol nm-connection-editor swww fish starship unzip git curl wget bash-completion fuzzel rustup go waybar neovim kitty fzf polkit chromium nerd-fonts swaync gtk2 gtk3 gtk4 sddm thefuck zoxide nodejs yarn npm gnome-keyring flatpak udiskie qt5-wayland qt6-wayland niri &&
+sudo pacman -S --noconfirm --needed base-devel stow nix fisher ghostty lua fd ripgrep yazi eza pipewire dunst noto-fonts-emoji libgtop bluez bluez-utils btop networkmanager brightnessctl python gnome-bluetooth-3.0 pacman-contrib gvfs tlp hyprland hyprpaper pavucontrol nm-connection-editor swww fish starship unzip git curl wget bash-completion fuzzel rustup go waybar neovim kitty fzf polkit chromium nerd-fonts swaync gtk2 gtk3 gtk4 sddm thefuck zoxide nodejs yarn npm gnome-keyring flatpak udiskie qt5-wayland qt6-wayland niri &&
 
 rustup default stable
 
@@ -30,17 +30,10 @@ cd ~/dotfiles
 stow . --adopt
 cd
 git clone git@github.com:Keremimo/nvim-lazy.git ~/.config/nvim
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher | fish
-fisher install nickeb96/puffer-fish | fish
-fisher install PatrickF1/fzf.fish | fish
+fisher install jorgebucaran/fisher
+fisher install nickeb96/puffer-fish
+fisher install PatrickF1/fzf.fish
+fisher install kidonng/nix.fish
 chsh -s /usr/bin/fish
 
 sudo flatpak override --filesystem=$HOME/.local/share/themes
-# Change to suite your flavor / accent combination
-export FLAVOR="mocha"
-export ACCENT="mauve"
-
-# Set the theme
-sudo flatpak override --env=GTK_THEME="catppuccin-${FLAVOR}-${ACCENT}-standard+default"
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix -o nix-install.sh
-sh ./nix-install.sh install
