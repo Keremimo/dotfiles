63c722392590a995db310d00829c91aee1f591cb
Kerem Kilic <code@kerem.tech>
2025-05-13 19:05:01 +0200

    Update dots

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 function fish_greeting
-  #kitten icat --align left ~/Pictures/okay.png
+    #kitten icat --align left ~/Pictures/okay.png
 end
 starship init fish | source
 fish_add_path /home/kerem/.spicetify
@@ -13,10 +13,10 @@ alias ls=eza
 thefuck --alias | source
 
 function starship_transient_prompt_func
-  starship module character
+    starship module character
 end
 function starship_transient_rprompt_func
-  starship module time
+    starship module time
 end
 starship init fish | source
 enable_transience
@@ -24,3 +24,4 @@ enable_transience
 set -Ux EDITOR nvim
 set fzf_preview_dir_cmd eza --all --color=always
 set fzf_fd_opts --hidden --max-depth 5
+set -g -x PIP_REQUIRE_VIRTUALENV true
